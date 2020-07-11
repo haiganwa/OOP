4707aefd4a2d92a65d04a6115e25f9c661c0b1df
hellohaigang <2452918712@qq.com>
2020-07-11 13:11:46 +0800

    OOP1\OOP1.sln OOP1\OOP1\OOP1.csproj OOP1\OOP1\Program.cs OOP1\OOP2\App.config OOP1\OOP2\Form1.Designer.cs OOP1\OOP2\Form1.cs OOP1\OOP2\Form1.resx OOP1\OOP2\OOP2.csproj OOP1\OOP2\Program.cs OOP1\OOP2\Properties\AssemblyInfo.cs OOP1\OOP2\Properties\Resources.Designer.cs OOP1\OOP2\Properties\Resources.resx OOP1\OOP2\Properties\Settings.Designer.cs OOP1\OOP2\Properties\Settings.settings OOP1\OOP2\mongodb.json OOP1\OOP2\packages.config

---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,76 @@
+﻿using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using SquirrelFramework.Domain.Model;
+using SquirrelFramework.Repository;
+using System;
+using JsonConvert = Newtonsoft.Json.JsonConvert;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            People Haigang = GeneratePeople();
+            People xiaohong = new People();
+            xiaohong.Age = 16;
+
+            People xiaoli = new People();
+            xiaoli.Age = 17;
+            People xiaozhang = new People();
+            xiaozhang.Age = 20;
+
+
+            PeopleCurRepository database = new PeopleCurRepository();
+            database.Add(Haigang);
+            database.Add(xiaozhang);
+            database.Add(xiaoli);
+            database.Add(xiaohong);
+     
+
+            //序列化
+            Console.WriteLine(JsonConvert.SerializeObject(database.GetAll()));
+            ////反序列化
+            //People Haigang2 = JsonConvert.DeserializeObject<People>("asdfafdadfa");
+            // Clone 深拷贝
+            //People Haigang2 = JsonConvert.DeserializeObject<People>(JsonConvert.SerializeObject(Haigang));
+        }
+          static  People GeneratePeople()
+        {
+            People Haigang = new People();
+            //素人
+
+            
+            Haigang.Gender = true;
+            Haigang.Age = 18;
+            Haigang.Name = "海钢";
+            Haigang.Birthday = new DateTime(2000, 10, 10, 10, 10, 36);
+            Haigang.HungerIndex = 100;
+            Haigang.Eat(99);
+
+            return Haigang;
+        }
+        
+    }
+    //数据库
+    class PeopleCurRepository : RepositoryBase<People> { }
+
+    class People : DomainModel
+    {       //成员方法
+        public void Eat(int foodsize) {
+            HungerIndex = HungerIndex + foodsize / 2;
+        }
+
+        //成员变量或成员属性
+        public int HungerIndex;
+        //唯一标识符
+        public string Name;
+        public int Age;
+        public bool Gender;
+        public DateTime Birthday;
+       
+
+    }
+}
---
+++ OOP1/OOP2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using SquirrelFramework.Domain.Model;
+using SquirrelFramework.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PeopleCurRepository database = new PeopleCurRepository();
+
+            this.dataGridView.DataSource = database.GetAll();
+
+        }
+
+        class PeopleCurRepository : RepositoryBase<People> { }
+
+        class People : DomainModel
+        {       //成员方法
+            public void Eat(int foodsize)
+            {
+                HungerIndex = HungerIndex + foodsize / 2;
+            }
+
+            //成员变量或成员属性
+            public int HungerIndex;
+            //唯一标识符
+            public string Name;
+            public int Age;
+            public bool Gender;
+            public DateTime Birthday;
+
+
+        }
+
+    }
+}
